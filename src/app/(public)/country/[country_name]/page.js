df710ea5f3325e01821c6f3665a516a063921c56
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2024-09-29 02:32:21 +0500

    changes

--- src/app/(public)/country/[country_name]/page.js
+++ src/app/(public)/country/[country_name]/page.js
@@ -2,14 +2,14 @@
 import { countries } from '@/app/data/countries';
 
 export default function CountryPage({ params }) {
-    const country = countries.find((c) => c.id === params.id);
+    // const country = countries.find((c) => c.id === params.id);
 
     return (
         <div>
 
-            <h1>{country.name}</h1>
-            <p>{country.population}</p>
-            <p>{country.capital}</p>
+            <h1>{params.country_name}</h1>
+            {/* <p>{country.population}</p>
+            <p>{country.capital}</p> */}
 
 
         </div>
