c79676e59b36967244b8c89c3eb70d5b7fb95f19
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2024-10-13 20:00:09 +0500

    update

--- src/app/(public)/country/page.tsx
+++ src/app/(public)/country/page.tsx
@@ -3,17 +3,25 @@ import Link from "next/link"
 
 
 const countryList = countries.slice(0, 5).map((item, i) => {
-    return <li key={i} className="hover:bg-slate-500 capitalize">
-        {i + 1} <Link className="p-4" href={`/country/${item.name}`}>{item.name.toLocaleLowerCase()}</Link>
-    </li>
+    return <Card key={i} item={item} />
 })
 
 export default function Country() {
     return (
         <>
             <h1 className="bg-gray-200 text-center p-4 hover:text-green-500 mb-2">Country List  </h1>
-            <ul>{countryList}</ul>
+            <ul>{countryList}
+
+            </ul>
 
         </>
+    )
+}
+
+function Card({ item }: { item: any }) {
+    return (
+        <li className="hover:bg-slate-500 capitalize">
+            <Link className="p-4" href={`/country/${item.name}`}>{item.name.toLocaleLowerCase()}</Link>
+        </li>
     )
 }
